db614bb4b4899abcea5b260c9064c7d9bcf3a5df
Andres Haehnel <ahaehnel@kiusys.com>
2023-04-18 21:20:19 -0300

    add title|description

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ models.Base.metadata.create_all(bind=engine)
 PREFIX = "/notification-service"
 
 app = FastAPI(
+    title="Notification service",
+    description="App for notify to subscribers by app_name and event name",
+    version="0.0.1",
     docs_url=f'{PREFIX}/docs',
     openapi_url=f'{PREFIX}/openapi.json'
 )
